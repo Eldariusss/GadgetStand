54abe80dcad0116c61283d17840cf66b87fa7676
Eldar <freeman-cs-705126@mail.ru>
2024-01-05 11:51:11 +0300

    editing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-new Glide('.glide').mount()
+const config = {
+    type: "carousel",
+    perView: 3,
+    breakpoints: {
+        800: {
+            perView:1
+        }
+    }
+};
+
+
+new Glide('.glide', config).mount();
